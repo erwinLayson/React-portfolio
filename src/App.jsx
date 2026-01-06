db75c00d9f8453b2a21bac2fd583455da609f6bc
erwin <erwin@gmail.com>
2026-01-06 15:36:34 +0900

    fix the app.jsx fiiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import { Home } from "./pages/Home";
-import { AboutMe } from "./pages/AboutMe";
-import { MyLanguages } from "./pages/MyLanguages";
-
-import { ROUTES } from "./constant/rotues";
 import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>  
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path={ROUTES.home} element={ <Home/>} />
-          <Route path={ROUTES.about} element={ <AboutMe/>} />
-          <Route path={ROUTES.techStack} element={ <MyLanguages/>} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <main>
+        <Home />
+      </main>
     </>
   )
 }
